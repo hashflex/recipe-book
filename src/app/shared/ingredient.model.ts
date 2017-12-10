0006e42f271b6a66af8e22cbdf5d92ccf17cd319
Amiya Sahu <amiya053@gmail.com>
2017-12-10 01:15:49 -0500

    working on components

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,9 @@
+/**
+ * The Ingredient model
+ */
+export class Ingredient {
+
+	// type script does its work of creating the variables 
+	// and assigning the values once the object is created
+	constructor(public name: string, public amount: number) {}
+}
