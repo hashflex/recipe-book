6c46925b8a311ba406826b1f53307faba259409c
Amiya Sahu <amiya053@gmail.com>
2017-12-09 23:22:42 -0500

    creating components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  
+  recipes: Recipe[] = [
+  	new Recipe("A test recipe 1", "test", "http://img1.cookinglight.timeinc.net/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/2016/09/main/1101-hawaiian-chicken-ck.jpg?itok=rTJgcebj"),
+  	new Recipe("A test recipe 2", "test", "http://img1.cookinglight.timeinc.net/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/2016/09/main/1101-hawaiian-chicken-ck.jpg?itok=rTJgcebj"),
+  	new Recipe("A test recipe 3", "test", "http://img1.cookinglight.timeinc.net/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/2016/09/main/1101-hawaiian-chicken-ck.jpg?itok=rTJgcebj")
+  ];
 
   constructor() { }
 
