57d65291c2a738cb1346bd99c1063b4c52d008bc
Amiya Sahu <amiya053@gmail.com>
2017-12-18 17:05:15 -0500

    small fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
-  loadedFeature: string = 'recipes';
+  loadedFeature = 'recipes';
 
   onNavigate(feature: string) {
     this.loadedFeature = feature;
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,9 +11,9 @@ export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
   recipes: Recipe[] = [
-    new Recipe('A test recipe 1', 'test', 'http://img1.cookinglight.timeinc.net/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/2016/09/main/1101-hawaiian-chicken-ck.jpg?itok=rTJgcebj'),
-    new Recipe('A test recipe 2', 'test', 'http://img1.cookinglight.timeinc.net/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/2016/09/main/1101-hawaiian-chicken-ck.jpg?itok=rTJgcebj'),
-    new Recipe('A test recipe 3', 'test', 'http://img1.cookinglight.timeinc.net/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/2016/09/main/1101-hawaiian-chicken-ck.jpg?itok=rTJgcebj')
+    new Recipe('Chicken', 'Chicken khurma', 'http://del.h-cdn.co/assets/16/24/640x320/landscape-1466100771-sweet-sour-chicken.png'),
+    new Recipe('Mutton', 'Mutton curry', 'https://2.bp.blogspot.com/--qSXjvPqwlk/WNIdQmvaN6I/AAAAAAAADAg/h0j9hytktVY2lFI_AuhBR1fE7UkfK3t0gCLcB/s1600/Mutton%2BRogan%2BJosh%2BRecipe.JPG'),
+    new Recipe('Fish', 'Nice fish curry', 'https://i.ytimg.com/vi/3NjKS_7xTXQ/hqdefault.jpg')
   ];
 
   constructor() {
